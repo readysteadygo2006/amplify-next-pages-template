11077d6ffaa2c172ab3cca452fe06ab57c52d2cb
Paul Grant <home.paul.grant@gmail.com>
2025-07-28 10:54:44 +0100

    added authenicator

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    
+  const { signOut } = useAuthenticator();
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -29,16 +36,16 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li 
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}>
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/gen2/start/quickstart/nextjs-pages-router/">
-          Review next steps of this tutorial.
-        </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
