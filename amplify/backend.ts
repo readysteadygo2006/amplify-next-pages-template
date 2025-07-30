cc7a980668f33d9ed941675f26c81ae3a2fbe29a
Paul Grant <home.paul.grant@gmail.com>
2025-07-30 12:30:30 +0100

    fix2

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-
+import { functionWithDataAccess } from './functions/data-access/resource';
 
 defineBackend({
   auth,
   data,
+  functionWithDataAccess,
 });
