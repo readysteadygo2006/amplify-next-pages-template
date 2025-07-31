10bb19a2f2945246d9693b88419e7b94fa61aca9
Paul Grant <home.paul.grant@gmail.com>
2025-07-31 13:07:26 +0100

    Fixes for env file

--- amplify/functions/data-access/resource.ts
+++ amplify/functions/data-access/resource.ts
@@ -1,5 +1,7 @@
 import { defineFunction } from '@aws-amplify/backend';
 
 export const functionWithDataAccess = defineFunction({
-  name: 'data-access',
+  environment: {
+    NAME: 'data-access',
+  }
 });
