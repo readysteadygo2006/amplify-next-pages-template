2ee4255a0d8abcf125831c901a7bfa05d767536d
Paul Grant <home.paul.grant@gmail.com>
2025-07-31 13:28:44 +0100

    added handler actual function

--- amplify/functions/data-access/handler.ts
+++ amplify/functions/data-access/handler.ts
@@ -11,5 +11,13 @@ Amplify.configure(resourceConfig, libraryOptions);
 const client = generateClient<Schema>();
 
 export const handler: Handler = async (event) => {
-  //
+  const { errors: createErrors, data: newTodo } = await client.models.Todo.create({
+    content: "My new todo",
+    isDone: false,
+  })
+
+
+  const { errors: listErrors, data: todos } = await client.models.Todo.list();
+
+  return event;
 };
