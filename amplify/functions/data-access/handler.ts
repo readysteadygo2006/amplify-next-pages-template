16a8faa37fae25c4776c1a0bf3471be5569ea72e
Paul Grant <home.paul.grant@gmail.com>
2025-08-01 07:34:59 +0100

    add logs

--- amplify/functions/data-access/handler.ts
+++ amplify/functions/data-access/handler.ts
@@ -12,9 +12,12 @@ const client = generateClient<Schema>();
 
 export const handler: Handler = async (event) => {
   console.log("event:", event);
+  let eventArgs = JSON.parse(event.queryStringParameters.params);
+  let eventContent = eventArgs.content; 
+  console.log("eventContent:", eventContent);
 
   const { errors: createErrors, data: newTodo } = await client.models.Todo.create({
-    content: "My new todo",
+    content: eventContent,
     isDone: false,
   })
 
